181685d7992cdfe65c8ca88592c60ee20ff614ec
jorgesierra <georgesierra@gmail.com>
2014-05-26 16:52:35 -0300

    fix default config

--- config/module.config.php
+++ config/module.config.php
@@ -3,7 +3,7 @@ return array(
     'tracy_debug' => array(
         'enabled' => TRUE,
         'mode' => FALSE,  // true = production|false = development|null = autodetect|IP address(es) csv/array
-        'log' => __DIR__ . '/../../../data/log',      // bool = enabled|Path to directory eg. data/logs
+        'log' => "",      // bool = enabled|Path to directory eg. data/logs
         'email' => "",    // in production mode notifies the recipient
         'strict' => TRUE, // bool = cause immediate death|int = matched against error severity
         'max_depth' => 3, // nested levels of array/object
